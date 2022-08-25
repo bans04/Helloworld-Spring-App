cdd85fcac7a9e1b44f3860fc364456d999913ce6
bans04 <bansodednyanadeo@gmail.com>
2022-08-25 12:44:45 +0530

    Added:-> Create a Helloworld Spring App to display Hello from BridgeLabz

---
+++ src/main/java/com/helloworldspringapp/HelloworldSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.helloworldspringapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldSpringAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Bridgelabz!");
+        SpringApplication.run(HelloworldSpringAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/helloworldspringapp/HelloworldSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworldspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
